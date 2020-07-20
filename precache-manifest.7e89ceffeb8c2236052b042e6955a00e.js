7414e2c9aaf1bf717fe2f81e7812b0e944216fb8
GuptaSiddhant <33697714+GuptaSiddhant@users.noreply.github.com>
2020-07-20 16:36:25 +0000

    Deploying to master from  @ b9e1b5382c9a4ae4a0d9031527ebaab4418e835f 🚀

---
+++ precache-manifest.7e89ceffeb8c2236052b042e6955a00e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82727184c674df61d19470c1e812a799",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5874ecf6493a0432e9d6",
+    "url": "/static/js/2.955c8949.chunk.js"
+  },
+  {
+    "revision": "bcc03b76fa0f77364072b5029c33948d",
+    "url": "/static/js/2.955c8949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3fa0c676523bf44fc43",
+    "url": "/static/js/main.b385447b.chunk.js"
+  },
+  {
+    "revision": "4178a590d621709143ec",
+    "url": "/static/js/runtime-main.40c3c141.js"
+  }
+]);
