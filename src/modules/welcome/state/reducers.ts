1db97b3ecb6992cd05253bcf441230109e542127
Siddhant Gupta <siddhant@fasolutions.com>
2020-08-31 12:15:14 +0300

    Add toast to redux example

--- src/modules/welcome/state/reducers.ts
+++ src/modules/welcome/state/reducers.ts
@@ -1,4 +1,6 @@
 import { CaseReducer, PayloadAction } from "@faharmony/state";
+import { addToast } from "@faharmony/core";
+import { uuid } from "@faharmony/helpers";
 import { IState } from "./store";
 
 type Reducer = CaseReducer<IState>;
@@ -6,10 +8,12 @@ type ReducerWithPayload<T extends any> = CaseReducer<IState, PayloadAction<T>>;
 
 const startLoading: Reducer = (state) => {
   state.loading = true;
+  addToast({ id: uuid(), title: "start loading" });
 };
 
 const stopLoading: Reducer = (state) => {
   state.loading = false;
+  addToast({ id: uuid(), title: "stop loading" });
 };
 
 const setLoading: ReducerWithPayload<boolean> = (state, { payload }) => {
