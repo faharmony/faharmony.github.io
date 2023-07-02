f16b11b9813a0e6740150368790ddd1e38754456
faharmonyuser <faharmonyuser@gmail.com>
2023-07-03 01:40:51 +0300

    add harmony dialog sample

--- src/modules/components/pages/MUI_Components/Samples/Dialog.tsx
+++ src/modules/components/pages/MUI_Components/Samples/Dialog.tsx
@@ -11,8 +11,9 @@ import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import Typography from "@mui/material/Typography";
 import React from "react";
+import { MUIDialog } from "@faharmony/core";
 
-const emails = ["username@gmail.com", "user02@gmail.com"]
+const emails = ["username@gmail.com", "user02@gmail.com"];
 
 export interface SimpleDialogProps {
   open: boolean;
@@ -21,15 +22,15 @@ export interface SimpleDialogProps {
 }
 
 function SimpleDialog(props: SimpleDialogProps) {
-  const { onClose, selectedValue, open } = props
+  const { onClose, selectedValue, open } = props;
 
   const handleClose = () => {
-    onClose(selectedValue)
-  }
+    onClose(selectedValue);
+  };
 
   const handleListItemClick = (value: string) => {
-    onClose(value)
-  }
+    onClose(value);
+  };
 
   return (
     <Dialog
@@ -39,17 +40,19 @@ function SimpleDialog(props: SimpleDialogProps) {
     >
       <DialogTitle id="simple-dialog-title">Set backup account</DialogTitle>
       <List>
-        {emails.map(email => (
+        {emails.map((email) => (
           <ListItem
             button
             onClick={() => handleListItemClick(email)}
             key={email}
           >
             <ListItemAvatar>
-              <Avatar sx={{
-                backgroundColor: blue[100],
-                color: blue[600],
-              }}>
+              <Avatar
+                sx={{
+                  backgroundColor: blue[100],
+                  color: blue[600],
+                }}
+              >
                 <PersonIcon />
               </Avatar>
             </ListItemAvatar>
@@ -70,21 +73,30 @@ function SimpleDialog(props: SimpleDialogProps) {
         </ListItem>
       </List>
     </Dialog>
-  )
+  );
 }
 
 export default function DialogExample() {
-  const [open, setOpen] = React.useState(false)
-  const [selectedValue, setSelectedValue] = React.useState(emails[1])
+  const [open, setOpen] = React.useState(false);
+  const [open2, setOpen2] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
 
   const handleClickOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
 
   const handleClose = (value: string) => {
-    setOpen(false)
-    setSelectedValue(value)
-  }
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
+  const handleClickOpen2 = () => {
+    setOpen2(true);
+  };
+
+  const handleClose2 = () => {
+    setOpen2(false);
+  };
 
   return (
     <div>
@@ -93,11 +105,28 @@ export default function DialogExample() {
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Open simple dialog
       </Button>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen2}>
+        Open Harmony MUI dialog
+      </Button>
       <SimpleDialog
         selectedValue={selectedValue}
         open={open}
         onClose={handleClose}
       />
+      <MUIDialog
+        id="MUIDialog-example"
+        open={open2}
+        title="MUI Dialog"
+        content="This is a MUI Dialog"
+        onConfirm={() => {
+          console.log("MUI Dialog confirmed");
+          handleClose2();
+        }}
+        onClosed={() => {
+          console.log("MUI Dialog cancelled");
+          handleClose2();
+        }}
+      />
     </div>
-  )
+  );
 }
