b46e9d1207df44acdd0811fb082ca77f343af02e
faharmonyuser <faharmonyuser@gmail.com>
2023-07-03 17:02:07 +0300

    add Toasts functions

--- src/modules/components/pages/MUI_Components/Samples/Snackbar.tsx
+++ src/modules/components/pages/MUI_Components/Samples/Snackbar.tsx
@@ -1,3 +1,4 @@
+import { addToastMUI, removeToastMUI } from "@faharmony/core";
 import CloseIcon from "@mui/icons-material/Close";
 import { Box, IconButton } from "@mui/material";
 import Button from "@mui/material/Button";
@@ -8,16 +9,18 @@ const action = (
   <Button color="secondary" size="small">
     lorem ipsum dolorem
   </Button>
-)
+);
 
 export default function SnackbarExample() {
   return (
-    <Box sx={{
-      maxWidth: 600,
-      "& > * + *": {
-        mt: 2,
-      },
-    }}>
+    <Box
+      sx={{
+        maxWidth: 600,
+        "& > * + *": {
+          mt: 2,
+        },
+      }}
+    >
       <SnackbarContent message="I love snacks." action={action} />
       <SnackbarContent
         message={
@@ -41,6 +44,82 @@ export default function SnackbarExample() {
         }
         action={action}
       />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        gap={2}
+        sx={{ width: "100%" }}
+      >
+        <Button
+          onClick={() =>
+            addToastMUI(
+              "I love candy. I love cookies. I love cupcakes. I love cheesecake. I love chocolate."
+            )
+          }
+        >
+          Open snackbar default
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={() =>
+            addToastMUI(
+              "I love candy. I love cookies. I love cupcakes. I love cheesecake. I love chocolate.",
+              { variant: "success" }
+            )
+          }
+        >
+          Success
+        </Button>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={() =>
+            addToastMUI(
+              "I love candy. I love cookies. I love cupcakes. I love cheesecake. I love chocolate.",
+              { variant: "info" }
+            )
+          }
+        >
+          Info
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={() =>
+            addToastMUI(
+              "I love candy. I love cookies. I love cupcakes. I love cheesecake. I love chocolate.",
+              { variant: "warning" }
+            )
+          }
+        >
+          Warning
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() =>
+            addToastMUI("Error message, click X button to close me -> ", {
+              variant: "error",
+              persist: true,
+              key: "error-snackbar",
+              action: (
+                <IconButton
+                  size="small"
+                  aria-label="close"
+                  color="inherit"
+                  onClick={() => removeToastMUI("error-snackbar")}
+                >
+                  <CloseIcon />
+                </IconButton>
+              ),
+            })
+          }
+        >
+          Error X
+        </Button>
+      </Box>
     </Box>
-  )
+  );
 }
